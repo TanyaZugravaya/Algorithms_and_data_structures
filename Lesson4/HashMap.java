ebc8012a414654540dd9000389a4a23b182a3218
Tanya <tanya@example.com>
2023-06-28 12:16:33 +0300

    HashMap

--- Lesson4/HashMap.java
+++ Lesson4/HashMap.java
@@ -1,2 +1,179 @@
-package org.example.CW.Algorithms_and_data_structures.Lesson4;public class HashMap {
+package org.example.CW.Algorithms_and_data_structures.Lesson4;
+
+public class HashMap {
+    class Entity {
+        int key;
+        int value;
+    }
+
+    class Basket {
+        Node head;
+
+        class Node {
+            Entity entity;
+            Node next;
+        }
+
+        public Integer find(int key) {
+            Node node = head;
+            while (node != null) {
+                if (node.entity.key == key) {
+                    return node.entity.value;
+                }
+                node = node.next;
+            }
+            return null;
+        }
+
+        public boolean push(Entity entity) {
+            Node node = new Node();
+            node.entity = entity;
+            if (head == null) {
+                head = node;
+            } else {
+                if (head.entity.key == entity.key) {
+                    return false;
+                } else {
+                    Node cur = head;
+                    while (cur.next != null) {
+                        if (cur.next.entity.key == entity.key) {
+                            return false;
+                        }
+                        cur = cur.next;
+                    }
+                    cur.next = node;
+                }
+            }
+            return true;
+        }
+
+        public boolean remove(int key) {
+            if (head != null) {
+                if (head.entity.key == key) {
+                    head = head.next;
+                    return true;
+                } else {
+                    Node cur = head;
+                    while (cur.next != null) {
+                        if (cur.next.entity.key == key) {
+                            cur.next = cur.next.next;
+                            return true;
+                        }
+                        cur = cur.next;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+
+
+    static final int INIT_SIZE = 16;
+
+    Basket[] baskets;
+
+    public HashMap() {
+        this(INIT_SIZE);
+    }
+
+    public HashMap(int size) {
+        baskets = new Basket[size];
+    }
+
+    private int getIndex(int key) {
+        return key % baskets.length;
+    }
+
+    public Integer find(int key) {
+        int index = getIndex(key);
+        Basket basket = baskets[index];
+        if (basket != null) {
+            return basket.find(key);
+        }
+        return null;
+    }
+
+    public boolean push(int key, int value) {
+        int index = getIndex(key);
+        Basket basket = baskets[index];
+        if (basket == null) {
+            basket = new Basket();
+            baskets[index] = basket;
+        }
+        Entity entity = new Entity();
+        entity.key = key;
+        entity.value = value;
+        return basket.push(entity);
+    }
+
+    public boolean remove(int key) {
+        int index = getIndex(key);
+        Basket basket = baskets[index];
+        if (basket != null) {
+            return basket.remove(key);
+        }
+        return false;
+    }
 }
+
+    class Tree{
+        Node root;
+        class Node{
+            int value;
+            Node left;
+            Node right;
+        }
+
+        public Node find(int value){
+            return find(root, value);
+        }
+
+        private Node find(Node node, int value){
+            if(node == null){
+                return null;
+            }
+            if(node.value == value){
+                return node;
+            }
+            if(node.value < value){
+                return find(node.right, value);
+            }else{
+                return find(node.left, value);
+            }
+        }
+
+        public void insert(int value){
+            if(root == null){
+                root = new Node();
+                root.value = value;
+            }else {
+                insert(root, value);
+                //root = balance(root);
+            }
+
+            //root.color = BLACK;
+        }
+
+        public void insert(Node node, int value){
+            if(node.value != value){
+                if(node.value < value){
+                    if(node.right == null){
+                        node.right = new Node();
+                        node.right.value = value;
+                        //node.right.color = RED;
+                    }else{
+                        insert(node.right, value);
+                    }
+                }else{
+                    if(node.left == null){
+                        node.left = new Node();
+                        node.left.value = value;
+                        //node.left.color = RED;
+                    }else{
+                        insert(node.left, value);
+                    }
+                }
+            }
+        }
+    }
